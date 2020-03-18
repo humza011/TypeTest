40b7c44e46ef76424b798fb48d3b0490d200b4fc
Humza Hasan <humzahasan2101@gmail.com>
2020-03-18 22:27:05 +0530

    Some modification

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 $( document ).ready(function() {
+  let quotesArray = [
+    "First, solve the problem. Then, write the code.",
+    "Experience is the name everyone gives to their mistakes.",
+    "In order to be irreplaceable, one must always be different.",
+    "Code is like humor. When you have to explain it, it’s bad.",
+    "Make it work, make it right, make it fast."]
   $("#tryagain").prop("disabled", true);
+  $("#actualtxt").text(quotesArray[Math.round(Math.random()*quotesArray.length)]);
 });
 
 const calcTime = () => {
-    var enteredText = Array.from($("#enteredtxt").val().trim());
-    var actualText = Array.from($("#actualtxt").text());
-    var lenFree = enteredText.length;
-    // var arrEntered = [];
-    // enteredText.forEach((letter,index) => {
-    //   arrEntered.push(enteredText[index]); 
-    // });
-
-    var actualLen = actualText.slice(0,lenFree);
-    
-    console.log(actualLen);
-  
-  }
+  var count = 0;
+  var input = document.getElementById('enteredtxt');
+  var enteredText = Array.from($("#enteredtxt").val().trim());
+  var actualText = Array.from($("#actualtxt").text());
+  var lenFree = enteredText.length;
+  var actualLen = actualText.slice(0,lenFree);
+  console.log(actualLen);
+}
   
   setTimeout(() => {
    console.log("After 5 seconds");
     calcTime();
+    $("#enteredtxt").prop("disabled", true);
     $("#tryagain").prop("disabled", false);
     console.log("End")
-  },5000)
+  },10000)
   
   
